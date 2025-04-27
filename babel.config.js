e65364c5d56f4bc5374e73ff902c6a68f224b1e5
kimhyojung <706shin1728@naver.com>
2025-04-27 14:41:09 +0900

    chore : expo- 초기세팅 수정

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
-
-  const presets = ["babel-preset-expo"];
-
   return {
-    presets,
-    plugins: ["expo-router/babel", "nativewind/babel"],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
   };
 };
